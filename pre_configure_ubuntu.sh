e38ccb11db8231962433fa93fd286976f769ebc6
cthurbson <security@userify.com>
2017-12-19 01:15:39 +0000

    Moved start script to pre_configure

--- pre_configure_ubuntu.sh
+++ pre_configure_ubuntu.sh
@@ -25,5 +25,61 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confd
     install -qqy ntp
 set -e
 
+if [ ! -f /etc/init.d/userify-server ]; then
+
+    # create the userify-server init script
+cat << "EOF" >/etc/init.d/userify-server
+#!/bin/bash
+# /etc/rc.d/init.d/userify-server
+# Userify Server startup script
+# This script is designed for maximum compatibility across all distributions,
+# including those that are running systemd and sysv
+
+### BEGIN INIT INFO
+# Provides:          userify-server
+# Required-Start:    $network $syslog
+# Required-Stop:     $network $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Start userify-server at boot time
+# Description:       Starts the Userify Server https://userify.com from /opt/userify-server.
+### END INIT INFO
+
+# chkconfig: 2345 20 80
+# description: Userify Server startup script
+
+case "$1" in
+    start)
+        # removed stop
+        echo -n "Starting Userify Server: "
+        /opt/userify-server/userify-start &
+        ;;
+    stop)
+        echo -n "Shutting down Userify Server: "
+        pkill userify-start
+        pkill userify-server
+        ;;
+    status)
+        pgrep userify-server
+        ;;
+    restart)
+        $0 stop; $0 start
+        ;;
+    reload)
+        $0 stop; $0 start
+        ;;
+    *)
+        echo "Usage: userify-server {start|stop|status|reload|restart}"
+        exit 1
+        ;;
+esac
+EOF
+    chmod +x /etc/init.d/userify-server
+    set +e
+    [ -f /usr/sbin/update-rc.d ] && sudo update-rc.d userify-server defaults
+    # $(command -v systemctl) && sudo systemctl enable userify-server
+    set -e
+fi
+
 # start it up
 sudo /opt/userify-server/userify-start 2>&1 |sudo tee /var/log/userify-server-initial.log &
